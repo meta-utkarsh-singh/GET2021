61c8722bfc0d014a5bcc98c94e51c88c8c74c6c7
Utkarsh Singh <utkarsh.singh@metacube.com>
2021-02-26 01:01:05 +0530

    Operation on string complete

---
+++ Assignment5/Question1/String operations/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String s = "Hello THere";
+		StringOperation op = new StringOperation();
+		System.out.println(op.reverse(s));
+		System.out.println(op.largestWord(s));
+		System.out.println(op.invertCase(s));
+	}
+
+}
