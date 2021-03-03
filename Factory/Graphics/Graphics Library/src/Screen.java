6d2745b54d59a17f2814093f721d6481af5913b3
Utkarsh Singh <utkarsh.singh@metacube.com>
2021-03-03 17:33:46 +0530

    test cases implemented

--- Factory/Graphics/Graphics Library/src/Screen.java
+++ Factory/Graphics/Graphics Library/src/Screen.java
@@ -20,7 +20,28 @@ public class Screen {
 	 * @param type
 	 * @param shapeParameter
 	 */
-	public void addShape(Point origin, String type, List<Double> shapeParameter){
+	public int addShape(Point origin, String type, List<Double> shapeParameter){
+		if(type == "Square"){
+			if(origin.getX() > screen.getX() || origin.getY() > screen.getY()
+					|| origin.getX() + shapeParameter.get(0)>screen.getX() || 
+					origin.getY() + shapeParameter.get(0) > screen.getY()){
+				return 0;
+			}
+		}
+		else if(type == "Rectangle"){
+			if(origin.getX() > screen.getX() || origin.getY() > screen.getY()
+					|| origin.getX() + shapeParameter.get(0)>screen.getX() || 
+					origin.getY() + shapeParameter.get(1) > screen.getY()){
+				return 0;
+			}
+		}
+		else if(type == "Circle"){
+			if(origin.getX() > screen.getX() || origin.getY() > screen.getY()
+					|| origin.getX() + shapeParameter.get(0)>screen.getX() || 
+					origin.getY() + shapeParameter.get(0) > screen.getY()){
+				return 0;
+			}
+		}
 		FactoryShape factory = new FactoryShape();
 		Shape shape = factory.createShape(type, origin, shapeParameter);
 		areaOfShapes.add(shape);
@@ -28,37 +49,41 @@ public class Screen {
 		timeStamp.add(shape);
 		distanceFromOrigin.add(shape);
 		shapeOnScreen.add(shape);
+		return 1;
 	}
 	/**
 	 * 
 	 * @param shape shape that needs to be deleted
 	 */
-	public void deleteShape(Shape shape){
+	public int deleteShape(Shape shape){
 		if(shapeOnScreen.contains(shape)){
 			shapeOnScreen.remove(shape);
 			areaOfShapes.remove(shape);
 			perimOfShapes.remove(shape);
 			distanceFromOrigin.remove(shape);
 			timeStamp.remove(shape);
+			return 1;
 		}
 		else{
 			System.out.println("Shape not found");
+			return 0;
 		}
 	}
 	/**
 	 * 
 	 * @param type type of shape that needs to be removed
 	 */
-	public void deleteShapeOnType(String type){
-		if(type != "Square" || type != "Circle" || type != "Rectangle"){
+	public int deleteShapeOnType(String type){
+		if(type != "Square" && type != "Circle" && type != "Rectangle"){
 			System.out.println("Shape not found");
-			return;
+			return 0;
 		}
 		for(int i = 0 ; i < shapeOnScreen.size() ; i++){
 			if(shapeOnScreen.get(i).shapeType() == type){
 				shapeOnScreen.remove(i);
 			}
 		}
+		return 1;
 	}
 	/**
 	 * 
@@ -94,5 +119,60 @@ public class Screen {
 			}
 		}
 		return isEnclosing;
+	}
+	/**
+	 * 
+	 * @return getter for shapeOnScreen array
+	 */
+	public List<Shape> getShapesOnScreen(){
+		List<Shape> shapeOnScreen = new ArrayList<Shape>();
+		for(int i = 0 ; i < this.shapeOnScreen.size() ; i++){
+			shapeOnScreen.add(this.shapeOnScreen.get(i));
+		}
+		return shapeOnScreen;
+	}
+	/**
+	 * 
+	 * @return getter for timeStamp array
+	 */
+	public List<Shape> getTimeStamp(){
+		List<Shape> timeStamp = new ArrayList<Shape>();
+		for(int i = 0 ; i < this.timeStamp.size() ; i++){
+			timeStamp.add(this.timeStamp.get(i));
+		}
+		return timeStamp;
+	}
+	/**
+	 * 
+	 * @return getter for distanceFromOrigin array
+	 */
+	public List<Shape> getDistanceFromOrigin(){
+		List<Shape> distanceFromOrigin = new ArrayList<Shape>();
+		for(int i = 0 ; i < this.distanceFromOrigin.size() ; i++){
+			distanceFromOrigin.add(this.distanceFromOrigin.get(i));
+		}
+		return distanceFromOrigin;
+	}
+	/**
+	 * 
+	 * @return getter for perimOfShape array
+	 */
+	public List<Shape> getPerimOfShapes(){
+		List<Shape> perimOfShapes = new ArrayList<Shape>();
+		for(int i = 0 ; i < this.perimOfShapes.size() ; i++){
+			perimOfShapes.add(this.perimOfShapes.get(i));
+		}
+		return perimOfShapes;
+	}
+	/**
+	 * 
+	 * @return getter for areaOfShape array
+	 */
+	public List<Shape> getAreaOfShapes(){
+		List<Shape> areaOfShapes = new ArrayList<Shape>();
+		for(int i = 0 ; i < this.areaOfShapes.size() ; i++){
+			areaOfShapes.add(this.areaOfShapes.get(i));
+		}
+		return areaOfShapes;
 	}
 }
