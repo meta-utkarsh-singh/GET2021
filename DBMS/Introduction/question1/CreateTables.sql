9bca3030147442ed9af98321e38382bc6aa43439
Utkarsh Singh <utkarsh.singh@metacube.com>
2021-03-16 12:43:19 +0530

    Columns modified

--- DBMS/Introduction/question1/CreateTables.sql
+++ DBMS/Introduction/question1/CreateTables.sql
@@ -17,11 +17,14 @@ create table SubCategory (SubCat_ID varchar(25) NOT NULL PRIMARY KEY,
                           FOREIGN KEY(Category_ID) references Category(Category_ID));
                           
 create table Product (Prod_ID varchar(25) NOT NULL PRIMARY KEY,
-                      Name varchar(25), Price int, Stocks int);
+                      Name varchar(25), Price int, Stocks int, AddDate DATETIME,
+                      Description varchar(50));
                       
 create table ProductCategory (Prod_ID varchar(25) references Product(Prod_ID),
                               SubCategory_ID varchar(25), 
-                              FOREIGN KEY(SubCategory_ID) references SubCategory(SubCat_ID));
+                              FOREIGN KEY(SubCategory_ID) references SubCategory(SubCat_ID),
+                              Category_ID varchar(25), FOREIGN KEY(Category_ID) references
+                              Category(Category_ID));
                               
 create table ProductImages (Prod_ID varchar(25), FOREIGN KEY(Prod_ID) 
                             references Product(Prod_ID), Image_Link varchar(100));
@@ -31,7 +34,7 @@ create table Orders(Order_ID varchar(25) NOT NULL PRIMARY KEY,Shopper_ID varchar
                     references Shopper(Shopper_ID), Status varchar(25), Shipping_Date DATETIME,
                     Delivery_Date DATETIME);
                     
-create table OrderProducts (Order_ID varchar(25) NOT NULL PRIMARY KEY , 
+create table OrderProducts (Order_ID varchar(25),FOREIGN KEY(Order_ID) references Orders(Order_ID) , 
                             Product_ID varchar(25),
                             FOREIGN KEY(Product_ID) references Product(Prod_ID));
                             
@@ -46,15 +49,18 @@ drop table OrderProducts;
 drop table Product;
 
 create table Product (Prod_ID varchar(25) NOT NULL PRIMARY KEY,
-                      Name varchar(25), Price int, Stocks int);
+                      Name varchar(25), Price int, Stocks int, AddDate DATETIME,
+                      Description varchar(50));
                       
 create table ProductCategory (Prod_ID varchar(25) references Product(Prod_ID),
                               SubCategory_ID varchar(25), 
-                              FOREIGN KEY(SubCategory_ID) references SubCategory(SubCat_ID));
+                              FOREIGN KEY(SubCategory_ID) references SubCategory(SubCat_ID),
+                              Category_ID varchar(25), FOREIGN KEY(Category_ID) references
+                              Category(Category_ID));
                               
 create table ProductImages (Prod_ID varchar(25), FOREIGN KEY(Prod_ID) 
                             references Product(Prod_ID), Image_Link varchar(100));
                             
-create table OrderProducts (Order_ID varchar(25) NOT NULL PRIMARY KEY , 
+create table OrderProducts (Order_ID varchar(25),FOREIGN KEY(Order_ID) references Orders(Order_ID) , 
                             Product_ID varchar(25),
                             FOREIGN KEY(Product_ID) references Product(Prod_ID));
