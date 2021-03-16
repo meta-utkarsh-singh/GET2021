c1b0528ba4ee23d3a9be2b629409fc9bebfd4a19
Utkarsh Singh <utkarsh.singh@metacube.com>
2021-03-16 17:35:36 +0530

    Create script implemented

---
+++ DBMS/CommandJoin/question1/Create_Script.sql
@@ -0,0 +1,68 @@
+drop database StoreFront;
+create database StoreFront;
+use StoreFront;
+
+create table User (User_ID varchar(25) NOT NULL PRIMARY KEY, Name varchar(25));
+
+create table Shopper(Shopper_ID varchar(25),FOREIGN KEY (Shopper_ID) 
+                     references User(User_ID), Name varchar(25));
+                     
+create table ShopperAddress(Shopper_ID varchar(25), FOREIGN KEY(Shopper_ID) references 
+                            Shopper(Shopper_ID), Address varchar(25));
+                            
+create table Category(Category_ID varchar(25) NOT NULL PRIMARY KEY, Name varchar(25));
+
+create table SubCategory (SubCat_ID varchar(25) NOT NULL PRIMARY KEY,
+                          Name varchar(25),Category_ID varchar(25),
+                          FOREIGN KEY(Category_ID) references Category(Category_ID));
+                          
+create table Product (Prod_ID varchar(25) NOT NULL PRIMARY KEY,
+                      Name varchar(25), Price int, Stocks int, AddDate DATETIME,
+                      Description varchar(50));
+                      
+create table ProductCategory (Prod_ID varchar(25) references Product(Prod_ID),
+                              SubCategory_ID varchar(25), 
+                              FOREIGN KEY(SubCategory_ID) references SubCategory(SubCat_ID),
+                              Category_ID varchar(25), FOREIGN KEY(Category_ID) references
+                              Category(Category_ID));
+                              
+create table ProductImages (Prod_ID varchar(25), FOREIGN KEY(Prod_ID) 
+                            references Product(Prod_ID), Image_Link varchar(100));
+                            
+create table Orders(Order_ID varchar(25) NOT NULL PRIMARY KEY,Shopper_ID varchar(25),
+                    FOREIGN KEY(Shopper_ID) 
+                    references Shopper(Shopper_ID), Status varchar(25), Placed_Date DATETIME,
+                    Delivery_Date DATETIME, Total int);
+                    
+create table OrderProducts (Order_ID varchar(25),FOREIGN KEY(Order_ID) references Orders(Order_ID) , 
+                            Product_ID varchar(25),
+                            FOREIGN KEY(Product_ID) references Product(Prod_ID),
+                            Status varchar(50), AddDate DATETIME);
+                            
+create table Admin(Admin_ID varchar(25) NOT NULL PRIMARY KEY, 
+                   User_ID varchar(25),FOREIGN KEY(User_ID) references User(User_ID));
+
+show tables;
+
+drop table ProductCategory;
+drop table ProductImages;
+drop table OrderProducts;
+drop table Product;
+
+create table Product (Prod_ID varchar(25) NOT NULL PRIMARY KEY,
+                      Name varchar(25), Price int, Stocks int, AddDate DATETIME,
+                      Description varchar(50));
+                      
+create table ProductCategory (Prod_ID varchar(25) references Product(Prod_ID),
+                              SubCategory_ID varchar(25), 
+                              FOREIGN KEY(SubCategory_ID) references SubCategory(SubCat_ID),
+                              Category_ID varchar(25), FOREIGN KEY(Category_ID) references
+                              Category(Category_ID));
+                              
+create table ProductImages (Prod_ID varchar(25), FOREIGN KEY(Prod_ID) 
+                            references Product(Prod_ID), Image_Link varchar(100));
+                            
+create table OrderProducts (Order_ID varchar(25),FOREIGN KEY(Order_ID) references Orders(Order_ID) , 
+                            Product_ID varchar(25),
+                            FOREIGN KEY(Product_ID) references Product(Prod_ID),
+                            Status varchar(50), AddDate DATETIME);
