1761029d8b8309447a121912f3b64dd053b9ecb2
Utkarsh Singh <utkarsh.singh@metacube.com>
2021-03-17 22:40:11 +0530

    Issues resolved

--- DBMS/CommandJoin/question1/Create_Script.sql
+++ DBMS/CommandJoin/question1/Create_Script.sql
@@ -2,45 +2,42 @@ drop database StoreFront;
 create database StoreFront;
 use StoreFront;
 
-create table User (User_ID varchar(25) NOT NULL PRIMARY KEY, Name varchar(25));
+create table User (ID INTEGER NOT NULL PRIMARY KEY, Name varchar(25));
 
-create table Shopper(Shopper_ID varchar(25),FOREIGN KEY (Shopper_ID) 
-                     references User(User_ID), Name varchar(25));
+create table Shopper(id INTEGER PRIMARY KEY ,user_id INTEGER,FOREIGN KEY (user_ID) 
+                     references User(ID), Name varchar(25), Email varchar(50));
                      
-create table ShopperAddress(Shopper_ID varchar(25), FOREIGN KEY(Shopper_ID) references 
-                            Shopper(Shopper_ID), Address varchar(25));
+create table ShopperAddress(Shopper_ID INTEGER PRIMARY KEY, FOREIGN KEY(Shopper_ID) references 
+                            Shopper(id), Address varchar(25));
                             
-create table Category(Category_ID varchar(25) NOT NULL PRIMARY KEY, Name varchar(25));
-
-create table SubCategory (SubCat_ID varchar(25) NOT NULL PRIMARY KEY,
-                          Name varchar(25),Category_ID varchar(25),
-                          FOREIGN KEY(Category_ID) references Category(Category_ID));
+create table Category(ID INTEGER PRIMARY KEY, Name varchar(25), sub_id INTEGER, 
+FOREIGN KEY(sub_id) REFERENCES category(id));
                           
-create table Product (Prod_ID varchar(25) NOT NULL PRIMARY KEY,
+create table Product (ID INTEGER NOT NULL PRIMARY KEY,
                       Name varchar(25), Price int, Stocks int, AddDate DATETIME,
-                      Description varchar(50));
+                      Description varchar(50), status varchar(25));
                       
-create table ProductCategory (Prod_ID varchar(25) references Product(Prod_ID),
-                              SubCategory_ID varchar(25), 
-                              FOREIGN KEY(SubCategory_ID) references SubCategory(SubCat_ID),
-                              Category_ID varchar(25), FOREIGN KEY(Category_ID) references
-                              Category(Category_ID));
+create table ProductCategory (ID INTEGER references Product(ID),
+                              SubCategory_ID INTEGER, 
+                              FOREIGN KEY(SubCategory_ID) references Category(Sub_ID),
+                              Category_ID INTEGER, FOREIGN KEY(Category_ID) references
+                              Category(ID));
                               
-create table ProductImages (Prod_ID varchar(25), FOREIGN KEY(Prod_ID) 
-                            references Product(Prod_ID), Image_Link varchar(100));
+create table ProductImages (Prod_ID INTEGER, FOREIGN KEY(Prod_ID) 
+                            references Product(ID), Image_Link varchar(100));
                             
-create table Orders(Order_ID varchar(25) NOT NULL PRIMARY KEY,Shopper_ID varchar(25),
+create table Orders(ID INTEGER NOT NULL PRIMARY KEY,Shopper_ID INTEGER,
                     FOREIGN KEY(Shopper_ID) 
-                    references Shopper(Shopper_ID), Status varchar(25), Placed_Date DATETIME,
+                    references Shopper(ID), Status varchar(25), Placed_Date DATETIME,
                     Delivery_Date DATETIME, Total int);
                     
-create table OrderProducts (Order_ID varchar(25),FOREIGN KEY(Order_ID) references Orders(Order_ID) , 
-                            Product_ID varchar(25),
-                            FOREIGN KEY(Product_ID) references Product(Prod_ID),
+create table OrderProducts (Order_ID INTEGER,FOREIGN KEY(Order_ID) references Orders(ID) , 
+                            Product_ID INTEGER,
+                            FOREIGN KEY(Product_ID) references Product(ID),
                             Status varchar(50), AddDate DATETIME);
                             
-create table Admin(Admin_ID varchar(25) NOT NULL PRIMARY KEY, 
-                   User_ID varchar(25),FOREIGN KEY(User_ID) references User(User_ID));
+create table Admin(Admin_ID INTEGER NOT NULL PRIMARY KEY, 
+                   User_ID INTEGER,FOREIGN KEY(User_ID) references User(ID));
 
 show tables;
 
@@ -49,20 +46,20 @@ drop table ProductImages;
 drop table OrderProducts;
 drop table Product;
 
-create table Product (Prod_ID varchar(25) NOT NULL PRIMARY KEY,
+create table Product (ID INTEGER NOT NULL PRIMARY KEY,
                       Name varchar(25), Price int, Stocks int, AddDate DATETIME,
-                      Description varchar(50));
+                      Description varchar(50), status varchar(25));
                       
-create table ProductCategory (Prod_ID varchar(25) references Product(Prod_ID),
-                              SubCategory_ID varchar(25), 
-                              FOREIGN KEY(SubCategory_ID) references SubCategory(SubCat_ID),
-                              Category_ID varchar(25), FOREIGN KEY(Category_ID) references
-                              Category(Category_ID));
+create table ProductCategory (prod_ID INTEGER references Product(ID),
+                              SubCategory_ID INTEGER, 
+                              FOREIGN KEY(SubCategory_ID) references Category(ID),
+                              Category_ID INTEGER, FOREIGN KEY(Category_ID) references
+                              Category(ID));
                               
-create table ProductImages (Prod_ID varchar(25), FOREIGN KEY(Prod_ID) 
-                            references Product(Prod_ID), Image_Link varchar(100));
+create table ProductImages (Prod_ID INTEGER, FOREIGN KEY(Prod_ID) 
+                            references Product(ID), Image_Link varchar(100));
                             
-create table OrderProducts (Order_ID varchar(25),FOREIGN KEY(Order_ID) references Orders(Order_ID) , 
-                            Product_ID varchar(25),
-                            FOREIGN KEY(Product_ID) references Product(Prod_ID),
+create table OrderProducts (Order_ID INTEGER,FOREIGN KEY(Order_ID) references Orders(ID) , 
+                            Product_ID INTEGER,
+                            FOREIGN KEY(Product_ID) references Product(ID),
                             Status varchar(50), AddDate DATETIME);
