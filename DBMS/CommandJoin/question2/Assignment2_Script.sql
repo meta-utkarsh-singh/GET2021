1e185fd5848af4df1d35c4b42993e2dc775d440a
Utkarsh Singh <utkarsh.singh@metacube.com>
2021-03-17 22:42:36 +0530

    Issues resolved

--- DBMS/CommandJoin/question2/Assignment2_Script.sql
+++ DBMS/CommandJoin/question2/Assignment2_Script.sql
@@ -1,18 +1,18 @@
-select p.Prod_ID,p.Name,Price,c.Name from Product p 
-left join productCategory pc on pc.PROD_ID = p.PROD_ID left join Category c 
-on c.Category_ID = pc.Category_id order by p.AddDate ASC;
+select p.ID,p.Name,Price,c.Name from Product p 
+left join productCategory pc on pc.PROD_ID = p.ID left join Category c 
+on c.ID = pc.Category_id WHERE p.status = "Active" order by p.AddDate ASC;
 
-select * from product p right join ProductImages pi on p.Prod_ID
-in (select PROD_ID from ProductImages) and p.Prod_ID = pi.Prod_ID;
+select * from product p right join ProductImages pi on p.ID
+in (select PROD_ID from ProductImages) and p.ID = pi.Prod_ID;
 
-select sc.SubCat_ID, IFNULL(sc.Name,'Top Category'),  c.Category_ID, c.Name from Category c
-left join SubCategory sc on c.Category_ID = sc.Category_ID order by 
+select c.Sub_ID, IFNULL(c.Name,'Top Category'),  c.ID, c.Name from Category c
+left join Category sc on c.ID = sc.ID order by 
 c.Name,sc.Name;
 
-select sc.SubCat_ID, sc.Name, c.Name from Category c left join SubCategory sc on 
-sc.Category_ID = c.Category_ID where sc.SubCat_ID not in (Select Category_ID from category);
+select c.Sub_ID, c.Name from Category c where c.id NOT IN 
+(SELECT sub_ID FROM category WHERE sub_id IS NOT NULL) AND c.sub_id IS NOT NULL;
 
 select p.Name, p.Price, p.Description from Product p, ProductCategory pc, Category c
-where p.Prod_ID = pc.Prod_ID and pc.Category_ID = c.Category_ID and c.Name = "Mobiles";
+where p.ID = pc.Prod_ID and pc.Category_ID = c.ID and c.Name = "Mobiles";
 
 select * from Product where Stocks < 50;
--- DBMS/CommandJoin/question2/Insert_Script.sql
+++ DBMS/CommandJoin/question2/Insert_Script.sql
@@ -4,31 +4,30 @@ insert into User VALUES (1, "A"), (2, "B"),
                         (3, "C"),(4, "D"),
                         (5, "E");
 
-insert into Shopper VALUES (1, "A"), (2, "B"),
-                        (3, "C"),(4, "D");
+insert into Shopper VALUES (1, 1, "A","A@"), (2, 2, "B", "B@"),
+                        (3, 3, "C", "C@"),(4, 4, "D", "D@");
 
 insert into ShopperAddress VALUES (1, "Address1"), (2, "Address2"),
                                   (3, "Address3"),(4, "Address4");
 
-insert into Category VALUES (1 , "Mobiles"), (2 , "Clothing"),
-                            (3 , "Grocery"), (4, "Utensils"),
-                            (5 , "Electronics"), (6 , "Laptops");
-                            
-insert into SubCategory VALUES (11 , "Kids", 2), (21, "Men", 2),
-                               (3, "Women", 2), (51, "Utility", 6),
-                               (61, "Gaming", 6), (71, "Business", 6),
-                               (81, "AC", 5), (9, "Refrigerator", 5);
+insert into Category VALUES (1 , "Mobiles",NULL), (2 , "Clothing", NULL),
+                            (3 , "Grocery",NULL), (4, "Utensils",NULL),
+                            (5 , "Electronics",NULL), (6 , "Laptops",NULL),
+                            (11 , "Kids", 2), (21, "Men", 2),
+                            (31, "Women", 2), (51, "Utility", 6),
+                            (61, "Gaming", 6), (71, "Business", 6),
+                            (81, "AC", 5), (9, "Refrigerator", 5);
 
-insert into Product Values (1, "Shirt", 1000, 50,CAST(N'2012-06-18 10:34:09.000' AS DateTime), "Shirt"),
-                           (2, "Pants", 1000, 50,CAST(N'2012-07-18 10:34:09.000' AS DateTime), "Shirt"),
-                           (3, "Nokia", 1000, 30,CAST(N'2012-06-18 10:35:09.000' AS DateTime), "Mobile"),
-                           (4, "Samsung", 1000, 50,CAST(N'2012-06-19 10:34:09.000' AS DateTime), "Mobile"),
-                           (5, "Stove", 1000, 50,CAST(N'2012-03-19 10:34:09.000' AS DateTime), "Utensil"),
-                           (6, "Dell", 1000, 50,CAST(N'2012-06-19 10:34:09.000' AS DateTime), "Laptop utility"),
-                           (7, "Acer", 1000, 50,CAST(N'2012-06-19 11:34:09.000' AS DateTime), "Laptop gaming"),
-                           (8, "Cooking oil", 1000, 50,CAST(N'2012-06-19 09:34:09.000' AS DateTime), "Grocery"),
-                           (9, "Samsung AC", 1000, 50,CAST(N'2012-06-19 12:34:09.000' AS DateTime), "AC"),
-                           (10, "LG", 1000, 41,CAST(N'2012-06-19 10:34:09.000' AS DateTime), "refrigerator");
+insert into Product Values (1, "Shirt", 1000, 50,CAST(N'2012-06-18 10:34:09.000' AS DateTime), "Shirt","Active"),
+                           (2, "Pants", 1000, 50,CAST(N'2012-07-18 10:34:09.000' AS DateTime), "Shirt","Active"),
+                           (3, "Nokia", 1000, 30,CAST(N'2012-06-18 10:35:09.000' AS DateTime), "Mobile","Active"),
+                           (4, "Samsung", 1000, 50,CAST(N'2012-06-19 10:34:09.000' AS DateTime), "Mobile","Active"),
+                           (5, "Stove", 1000, 50,CAST(N'2012-03-19 10:34:09.000' AS DateTime), "Utensil","Active"),
+                           (6, "Dell", 1000, 50,CAST(N'2012-06-19 10:34:09.000' AS DateTime), "Laptop utility","Active"),
+                           (7, "Acer", 1000, 50,CAST(N'2012-06-19 11:34:09.000' AS DateTime), "Laptop gaming","Active"),
+                           (8, "Cooking oil", 1000, 50,CAST(N'2012-06-19 09:34:09.000' AS DateTime), "Grocery","Active"),
+                           (9, "Samsung AC", 1000, 50,CAST(N'2012-06-19 12:34:09.000' AS DateTime), "AC","Active"),
+                           (10, "LG", 1000, 41,CAST(N'2012-06-19 10:34:09.000' AS DateTime), "refrigerator","Active");
                            
 insert into ProductImages VALUES (1,"image1"),(1,"image2"),
                                  (2,"image1"),(3,"image1"),
@@ -42,7 +41,8 @@ insert into ProductCategory VALUES (1, 11, 2), (2, 21, 2),
                                    (3, NULL, 1), (4, NULL, 1),
                                    (5, NULL, 4), (6, 51, 6),
                                    (7, 61, 6),(8, NULL, 3),
-                                   (9, 81, 5),(10, 9, 5);
+                                   (9, 81, 5),(10, 9, 5),
+                                   (7,NULL,5),(6,NULL,5);
 
 insert into Orders VALUES 
 (1, 1, "Shipped", CAST(N'2021-03-10 11:34:09.000' AS DateTime), 
