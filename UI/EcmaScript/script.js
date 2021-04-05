89f5be8face7a60544c0e592685975c1742f66a7
Utkarsh Singh <utkarsh.singh@metacube.com>
2021-04-05 10:52:15 +0530

    Issues resolved

--- UI/EcmaScript/script.js
+++ UI/EcmaScript/script.js
@@ -20,6 +20,8 @@ let motorcycleYearly = 1000;
 let fourWheelerDaily = 20;
 let fourWheelerMonthly = 500;
 let fourWheelerYearly = 3500;
+let emailEntered = "";
+let flag = 0;
 
 /** Helper method to check password validation */
 
@@ -39,7 +41,7 @@ let validatePass = (empPassEntered) => {
             passLower = true;
         }
     } 
-    if(passNum && passUpper && passLower) {
+    if(passNum && passUpper && passLower && empPassEntered.length >= 8) {
         return true;
     }
     return false;
@@ -49,6 +51,9 @@ let validatePass = (empPassEntered) => {
 
 let validatePhone = () => {
     let valid = true;
+    if(String(empPhoneEntered).length < 8) {
+        return false;
+    }
     for(let i = 0 ; i < empPhoneEntered.length ; i++){
         if(!isFinite(empPhoneEntered.charAt(i))) {
             valid = false;
@@ -56,6 +61,44 @@ let validatePhone = () => {
         }
     }
     return valid;
+}
+
+/**
+ * Helper method to check validity of first name
+ */
+
+let validateName = () => {
+    if(empNameEntered.length < 2) {
+        return false;
+    }
+    for(let i = 0 ; i < empNameEntered.length ; i++) {
+        if(isFinite(empNameEntered.charAt(i))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * Helper method to check validity of email
+ */
+
+let validateMail = () => {
+    const regexMail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexMail.test(String(emailEntered).toLowerCase());
+}
+
+/**
+ * Helper method to check if strength of password is normal
+ */
+
+let checkStrengthNormal = () => {
+    for(let i = 0 ; i < empPassEntered.length ; i++) {
+        if(isFinite(empPassEntered.charAt(i))) {
+            return true;
+        }
+    }
+    return false;
 }
 
 /** getter for forms and pricing section */
@@ -138,6 +181,7 @@ let priceYearlyCycle = document.getElementById("price-yearly-cycle");
 let getPassCycle = document.getElementById("pass-cycle");
 let cycleInr = document.getElementById("c-inr");
 let cycleYen = document.getElementById("c-yen");
+let cycleDollar = document.getElementById("c-dollar");
 
 /** getter for component of pricing motorcycle */
 
@@ -147,6 +191,7 @@ let priceYearlyMotorcycle = document.getElementById("price-yearly-motorcycle");
 let getPassMotorMotorcycle = document.getElementById("pass-motorcycle");
 let motorcycleInr = document.getElementById("m-inr");
 let motorcycleYen = document.getElementById("m-yen");
+let motorcycleDollar = document.getElementById("m-dollar");
 
 /** getter for component of pricing four wheeler */
 
@@ -156,6 +201,7 @@ let priceYearlyFourWheeler = document.getElementById("price-yearly-four-wheelers
 let getPassFourWheeler = document.getElementById("pass-four-wheeler");
 let fourWheelInr = document.getElementById("f-inr");
 let fourWheelYen = document.getElementById("f-yen");
+let fourWheelDollar = document.getElementById("f-dollar");
 
 /**Sets display none initially at page load time */
 
@@ -167,14 +213,25 @@ pricing.classList.add("d-none");
 /** onclick event : display the form when the heading is clicked */
 
 empHead.onclick = () => {
-    if(countClickEmpHead % 2 == 0) {
+    if(countClickEmpHead % 2 == 0 && flag == 0) {
         empForm.classList.remove("d-none");
         vehForm.classList.add("d-none");
         feedbackForm.classList.add("d-none");
+        empHead.classList.add("d-none");
         countClickVehHead = 0;
         countClickFeedbackHead = 0;
         countClickEmpHead = 1;
+        flag = 1;
     }    
+    else if(countClickEmpHead % 2 == 0 && flag==1) {
+        empForm.classList.remove("d-none");
+        vehForm.classList.add("d-none");
+        feedbackForm.classList.add("d-none");
+        vehHead.classList.remove("d-none");
+        countClickVehHead = 0;
+        countClickFeedbackHead = 0;
+        countClickEmpHead = 1;
+    } 
     else {
         empForm.classList.add("d-none");
         countClickEmpHead = 0;
@@ -189,6 +246,7 @@ vehHead.onclick = () => {
         countClickFeedbackHead = 0;
         countClickEmpHead = 0;
         countClickVehHead = 1;
+        vehHead.classList.add("d-none");
     }    
     else {
         vehForm.classList.add("d-none");
@@ -215,55 +273,55 @@ feedbackHead.onclick = () => {
 
 iEmpFirstName.onkeydown = (event)=>{
     countPressKey = countPressKey + 1;
-    if(isFinite(event.key)){
-        alert("Please do not enter numbers");
-    }
-    else if(event.key == "Enter" && countPressKey < 3){
-        alert("Enter valid name");
-    }
-    else if(event.key == "Enter") {
+    empNameEntered = document.getElementById("i-firstname").value;
+    if(event.key == "Enter" && validateName()) {
         empFname.classList.add("d-none");
-        gender.innerText ="Please select your gender "+empNameEntered;
+        gender.innerText ="Hello "+empNameEntered+", please select your gender.";
         empGender.classList.remove("d-none");
     }
-    if(event.key!="Enter") {
-        empNameEntered = empNameEntered + event.key;
+    else if(event.key == "Enter") {
+        alert("Please enter a valid name");
     }
 }
 
 empGender.onkeydown = (event) => {
     empGender.classList.add("d-none");
-    lEmail.innerHTML = "Please enter email " + empNameEntered;
+    lEmail.innerHTML = "Hello " + empNameEntered + ", please enter your email.";
     empEmail.classList.remove("d-none");
 }
 
 iEmpMail.onkeydown = (event) => {
-    if(event.key == "Enter") {
+    emailEntered = document.getElementById("i-mail").value;
+    if(event.key == "Enter" && validateMail()) {
         empEmail.classList.add("d-none");
-        lPassword.innerHTML = "Please enter your password "+empNameEntered;
+        lPassword.innerHTML = "Hello " + empNameEntered + ", please enter your password";
         empPassword.classList.remove("d-none");
+    }
+    else if (event.key == "Enter") {
+        alert("Please enter a valid email");
     }
 }
 
-iEmpPassword.onkeydown = (event) => {
+iEmpPassword.onkeyup = (event) => {
     let ch = event.key;
     empPassEntered = document.getElementById("i-pwd").value;
-    if(!validatePass(empPassEntered) || empPassEntered.length < 10) {
+    console.log(empPassEntered);
+    if(!validatePass(empPassEntered) && !checkStrengthNormal()) {
         iEmpPassword.classList.remove("border-orange");
         iEmpPassword.classList.remove("border-green");
         iEmpPassword.classList.add("border-red");
     }
-    else if(validatePass(empPassEntered) && empPassEntered.length == 10) {
+    else if(!validatePass(empPassEntered) && checkStrengthNormal()) {
         iEmpPassword.classList.remove("border-green");
-        iEmpPassword.classList.add("border-orange");
         iEmpPassword.classList.remove("border-red");
+        iEmpPassword.classList.add("border-orange");
     }
-    else if(validatePass(empPassEntered) && empPassEntered.length > 10) {
+    else if(validatePass(empPassEntered)) {
         iEmpPassword.classList.remove("border-red");
         iEmpPassword.classList.remove("border-orange");
         iEmpPassword.classList.add("border-green");
     }
-    if(ch == "Enter" && validatePass(empPassEntered) && empPassEntered.length >= 8) {
+    if(ch == "Enter" && validatePass(empPassEntered)) {
         empPassword.classList.add("d-none");
         lConfirmPassword = "Please re-enter the password for confirmation "+empNameEntered; 
         empConfirmPassword.classList.remove("d-none");
@@ -298,8 +356,9 @@ iEmpPhone.onkeydown = (event) => {
         empForm.classList.add("d-none");
         countClickEmpHead = 0;
         empFname.classList.remove("d-none");
-        alert("Hello, "+empNameEntered+" your registration ID is"+regId);
-        window.location.reload();
+        vehHead.classList.remove("d-none");
+        empHead.classList.remove("d-none");
+        alert("Registration Successful!!\n"+"Hello, "+empNameEntered+" your registration ID is"+regId);
     }
 }
 
@@ -346,11 +405,13 @@ iVehicleNumber.onkeydown = (event) => {
 }
 
 vehicleIdentification.onkeydown = (event) => {
+    vehHead.classList.add("d-none");
     if(event.key == "Enter") {
         vehicleIdentification.classList.add("d-none");
         vehForm.classList.add("d-none");
         companyName.classList.remove("d-none");
         pricing.classList.remove("d-none");
+        vehHead.classList.remove("d-none");
         if(vehicleSelected == "Car" || vehicleSelected == "truck") {
             pricingFourWheeler.classList.remove("d-none");
         }
@@ -362,38 +423,74 @@ vehicleIdentification.onkeydown = (event) => {
 
 priceDailyFourWheeler.onclick = (event) => {
     passSelected = "daily4";
+    getPassFourWheeler.disabled = false;
+    priceDailyFourWheeler.style.color = "red";
+    priceMonthlyFourWheeler.style.color = "black";
+    priceYearlyFourWheeler.style.color = "black";
 }
 
 priceMonthlyFourWheeler.onclick = (event) => {
     passSelected = "monthly4";
+    getPassFourWheeler.disabled = false;
+    priceDailyFourWheeler.style.color = "black";
+    priceMonthlyFourWheeler.style.color = "red";
+    priceYearlyFourWheeler.style.color = "black";
 }
 
 priceYearlyFourWheeler.onclick = (event) => {
     passSelected = "yearly4";
+    getPassFourWheeler.disabled = false;
+    priceDailyFourWheeler.style.color = "black";
+    priceMonthlyFourWheeler.style.color = "black";
+    priceYearlyFourWheeler.style.color = "red";
 }
 
 priceDailyCycle.onclick = (event) => {
     passSelected = "dailyC";
+    getPassCycle.disabled = false;
+    priceDailyCycle.style.color = "red";
+    priceMonthlyCycle.style.color = "black";
+    priceYearlyCycle.style.color = "black";
 }
 
 priceMonthlyCycle.onclick = (event) => {
     passSelected = "monthlyC";
+    getPassCycle.disabled = false;
+    priceDailyCycle.style.color = "black";
+    priceMonthlyCycle.style.color = "red";
+    priceYearlyCycle.style.color = "black";
 }
 
 priceYearlyCycle.onclick = (event) => {
     passSelected = "yearlyC";
+    getPassCycle.disabled = false;
+    priceDailyCycle.style.color = "black";
+    priceMonthlyCycle.style.color = "black";
+    priceYearlyCycle.style.color = "red";
 }
 
 priceDailyMotorcycle.onclick = (event) => {
     passSelected = "dailyM";
+    getPassMotorMotorcycle.disabled = false;
+    priceDailyMotorcycle.style.color = "red";
+    priceMonthlyMotorcycle.style.color = "black";
+    priceYearlyMotorcycle.style.color = "black";
 }
 
 priceMonthlyMotorcycle.onclick = (event) => {
     passSelected = "monthlyM";
+    getPassMotorMotorcycle.disabled = false;
+    priceDailyMotorcycle.style.color = "black";
+    priceMonthlyMotorcycle.style.color = "red";
+    priceYearlyMotorcycle.style.color = "black";
 }
 
 priceYearlyMotorcycle.onclick = (event) => {
     passSelected = "yearlyM";
+    getPassMotorMotorcycle.disabled = false;
+    priceDailyMotorcycle.style.color = "black";
+    priceMonthlyMotorcycle.style.color = "black";
+    priceYearlyMotorcycle.style.color = "red";
 }
 
 getPassCycle.onclick = (event) => {
@@ -467,4 +564,22 @@ motorcycleYen.onclick = (event) => {
     priceDailyMotorcycle.innerHTML = "YEN "+motorcycleDaily*80+" Daily";
     priceMonthlyMotorcycle.innerHTML = "YEN "+motorcycleMonthly*80+" Monthly";
     priceYearlyMotorcycle.innerHTML = "YEN "+motorcycleYearly*80+" Yearly"; 
+}
+
+fourWheelDollar.onclick = (event) => {
+    priceDailyFourWheeler.innerHTML = "$ "+fourWheelerDaily+" Daily";
+    priceMonthlyFourWheeler.innerHTML = "$ "+fourWheelerMonthly+" Monthly";
+    priceYearlyFourWheeler.innerHTML = "$ "+fourWheelerYearly+" Yearly"; 
+}
+
+cycleDollar.onclick = (event) => {
+    priceDailyCycle.innerHTML = "$ "+cycleDaily+" Daily";
+    priceMonthlyCycle.innerHTML = "$ "+cycleMonthly+" Monthly";
+    priceYearlyCycle.innerHTML = "$ "+cycleYearly+" Yearly"; 
+}
+
+motorcycleDollar.onclick = (event) => {
+    priceDailyMotorcycle.innerHTML = "$ "+motorcycleDaily+" Daily";
+    priceMonthlyMotorcycle.innerHTML = "$ "+motorcycleMonthly+" Monthly";
+    priceYearlyMotorcycle.innerHTML = "$ "+motorcycleYearly+" Yearly"; 
 }
